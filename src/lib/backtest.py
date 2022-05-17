a14801623db0e571b8c063632f181957aa7df198
gaborotta <10.daaaan.24@gmail.com>
2022-05-18 01:01:03 +0900

    docstring記述

--- src/lib/backtest.py
+++ src/lib/backtest.py
@@ -1,7 +1,7 @@
 # import os
 # os.chdir("src")
 
-from typing import Tuple
+from typing import Tuple, List
 from mlflow.entities import Experiment, Run
 from numpy import float64
 from lib import util
@@ -16,25 +16,30 @@ import mlflow
 
 class Order():
     def __init__(self, type: str, idx: int, volume: float, price: float) -> None:
+        """オーダークラス
+
+        Args:
+            type (str): 売買タイプ（買い："buy"、売り："sell"）
+            idx (int): オーダーが発生するidx
+            volume (float): オーダーしたvolume
+            price (float): オーダー時の価格
+        """
         self.type = type
         self.idx = idx
         self.volume = volume
         self.price = price
+
+        # 売買金額
         self.amount = volume * price
         pass
 
 
 class BackTest:
-    """_summary_
-    """
-
     def __init__(self, back_test_setting: IBackTestSetting) -> None:
-        """_summary_
+        """バックテストを実行するクラス
 
         Args:
-            experiment_name (str): mlflowの実験名。MLBotがデフォルト
-            rule_name (str): 検証するルールの名称
-            version (str): ルールのバージョン
+            back_test_setting (IBackTestSetting): バックテストの設定インスタンス。IBackTestSettingを継承しているクラスであること。
         """
         self.initial_balance = back_test_setting.initial_balance
         self.experiment_name = back_test_setting.experiment_name
@@ -65,10 +70,30 @@ class BackTest:
         return experiment, run
 
     def _finish_mlflow(self):
+        """MLFlowを終了する。
+        """
         mlflow.end_run()
 
-    def _buy(self, context: Context, now_idx: int, volume: float64, price: float64):
+    def _buy(self, context: Context, now_idx: int, volume: float64, price: float64) -> dict:
+        """買う関数
 
+        Args:
+            context (Context): コンテキスト
+            now_idx (int): 現時点のidx
+            volume (float64): 買うボリューム
+            price (float64): 現時点の価格
+
+        Returns:
+            dict: 購買結果
+            dict(
+                idx : 現時点のインデックス
+                buy_success_flg :買いが成功したフラグ
+                amount :購買金額
+                balance :現時点の残高
+                total_buy_count :現時点での合計購買回数
+                total_buy_amount :現時点での合計購買金額
+            )
+        """
         amount = volume * price
 
         buy_success_flg = False
@@ -90,14 +115,38 @@ class BackTest:
             total_buy_amount=context.total_buy_amount,
         )
 
-    def _sell(self, context: Context, now_idx: int, price: float64):
+    def _sell(self, context: Context, now_idx: int, sell_order_list: List[Order], price: float64) -> List[dict]:
+        """売る関数
 
+        Args:
+            context (Context): コンテキスト
+            now_idx (int): 現時点のインデックス
+            sell_order_list (List[Order]): 売りたいオーダーのリスト
+            price (float64): 現時点の価格
+
+        Returns:
+            List[dict]: 売った結果のリスト
+            dict(
+                idx : 現時点のインデックス
+                buy_idx : 購入時のオーダーのインデックス
+                sell_success_flg :売り成功フラグ
+                amount :売却金額
+                sell_return :対象の購入オーダーに対するリターン
+                balance :残高
+                total_sell_count :現時点の合計売却回数
+                total_sell_amount :現時点の合計売却金額
+                total_return_amount :現時点のトータルリターン
+            )
+        """
         sell_success_flg = False
         buy_idx = None
         amount = 0
         sell_return = 0
-        if len(context.buy_status) > 0:
-            latest_buy = context.buy_status.pop(-1)
+
+        selled_order_list = []
+        for sell_order in sell_order_list:
+
+            latest_buy = sell_order
             buy_idx = latest_buy.idx
             amount = latest_buy.volume * price
             sell_return = amount - latest_buy.amount
@@ -108,20 +157,35 @@ class BackTest:
             context.total_return_amount += sell_return
             sell_success_flg = True
 
-        return dict(
-            idx=now_idx,
-            buy_idx=buy_idx,
-            sell_success_flg=sell_success_flg,
-            amount=amount,
-            sell_return=sell_return,
-            balance=context.balance,
-            total_sell_count=context.total_sell_count,
-            total_sell_amount=context.total_sell_amount,
-            total_return_amount=context.total_return_amount,
-        )
+            selled_order_list.append(
+                dict(
+                    idx=now_idx,
+                    buy_idx=buy_idx,
+                    sell_success_flg=sell_success_flg,
+                    amount=amount,
+                    sell_return=sell_return,
+                    balance=context.balance,
+                    total_sell_count=context.total_sell_count,
+                    total_sell_amount=context.total_sell_amount,
+                    total_return_amount=context.total_return_amount,
+                )
+            )
+            context.buy_status.remove(sell_order)
 
-    def run_backtest(self):
+        return selled_order_list
 
+    def run_backtest(self) -> dict:
+        """バックテストを実行する関数
+
+        Returns:
+            dict: 実行結果
+            dict(
+                success_flg :データ読み込み成功フラグ
+                res_list :バーごとの判定結果のリスト
+                buy_res_list :購買結果のリスト
+                sell_res_list :売却結果のリスト
+            )
+        """
         # 設定
         bt_stng = self.back_test_setting
 
@@ -139,12 +203,14 @@ class BackTest:
         next_buy_idx = None
         next_sell_idx = None
         latest_buy_judge_res = None
+        latest_sell_judge_res = None
         for idx in range(start_idx, len(ohlcv_data)):
             # 購買する場合
             if idx == next_buy_idx:
                 buy_res = self._buy(
                     context=self.context, now_idx=idx,
-                    volume=latest_buy_judge_res["buy_volume"], price=ohlcv_data.loc[idx][bt_stng.price_col]
+                    volume=latest_buy_judge_res["buy_volume"],
+                    price=ohlcv_data.loc[idx][bt_stng.price_col]
                 )
                 next_buy_idx = None
                 latest_buy_judge_res = None
@@ -154,10 +220,12 @@ class BackTest:
             elif idx == next_sell_idx:
                 sell_res = self._sell(
                     context=self.context, now_idx=idx,
+                    sell_order_list=latest_sell_judge_res["sell_order_list"],
                     price=ohlcv_data.loc[idx][bt_stng.price_col]
                 )
                 next_sell_idx = None
-                sell_res_list.append(sell_res)
+                latest_sell_judge_res = None
+                sell_res_list += sell_res
 
             # 次の売買タイミングの取得
             res = bt_stng.judge_buysell_timing(now_idx=idx, ohlcv_data=ohlcv_data, context=self.context)
@@ -169,6 +237,7 @@ class BackTest:
                 latest_buy_judge_res = res
             elif res["sell_flg"]:
                 next_sell_idx = res["sell_idx"]
+                latest_sell_judge_res = res
 
         # mlflow.log_figure(fig, "figure.png")
         # mlflow.log_param("k", list(range(2, 50)))
